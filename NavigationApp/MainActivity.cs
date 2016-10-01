279b690490114ea19c2cd553f06811aa4bcc76d3
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2016-10-01 12:14:32 +0530

    Add project files.

---
+++ NavigationApp/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace NavigationApp
+{
+    [Activity(Label = "Navigation App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var btnNavigate = FindViewById<Button>(Resource.Id.btnNavigate);
+            btnNavigate.Click += BtnNavigate_Click;
+        }
+
+        private void BtnNavigate_Click(object sender, System.EventArgs e)
+        {
+            // Simple option
+            StartActivity(typeof(SecondActivity));
+        }
+    }
+}
+
---
+++ NavigationApp/SecondActivity.cs
@@ -0,0 +1,17 @@
+using Android.App;
+using Android.OS;
+
+namespace NavigationApp
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Second);
+        }
+    }
+}
